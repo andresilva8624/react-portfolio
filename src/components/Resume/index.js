79d84e6c9030bc80ac71464e825337f65ba093bc
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-12-12 21:31:19 -0800

    updated color

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/andresilva8624">resume</a>
+        Download my <a href="http://www.linkedin.com/in/andresilva8624">Resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
