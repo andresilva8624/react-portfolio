1ffafe17b67df7590e6ad92c9ebcdaef77db1b0f
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-12-14 12:31:53 -0800

    updated json

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,7 +25,6 @@ function Footer() {
       (
         <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
       )
-
       )}
     </footer>
   );
