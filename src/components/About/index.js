b08b3d13bd68e3e1afc389c02af3725f73912a1b
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-12-14 18:34:08 -0800

    updated profile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        <img src={Image} alt="wooden background"></img>
         </div>
         <p>
         Born and raised in Porto Alegre, Brazil, I moved to Los Angeles in 2006 looking to pursue a career in music. I've been playing guitar for over 20 years but also have I've worked on many different
@@ -15,7 +15,7 @@ function About() {
                     girlfriend and my dog Django in the City Of Angels. I love to travel and have a good time with my
                     friends.
         </p>
-        <img src={Image} alt="wooden background"></img>
+       
         <p>
         I'm
                     currently studying software and web development at UCLA Bootcamp. Very excited to learn this new
