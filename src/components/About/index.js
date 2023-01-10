acde51bbeebb9264d66fe6b31b654aa60c23f8e5
andresilva8624@gmail.com <andresilva8624@gmail.com>
2023-01-10 15:51:40 -0800

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,22 +9,18 @@ function About() {
         <div className="profile-img my-5">
         <img src={Image} alt="wooden background"></img>
         </div>
-        <p>
-        Born and raised in Porto Alegre, Brazil, I moved to Los Angeles in 2006 looking to pursue a career in music. I've been playing guitar for over 20 years but also have I've worked on many different
-                    industries.
-                    I have a passion for working hard and making a difference on the community. I live with my
-                    girlfriend and my dog Django in the City Of Angels. I love to travel and have a good time with my
-                    friends.
+        <p>        Personable and passionate Full Stack Web Developer with a background in sales and customer service to provide unique perspectives on how users interact with web applications and software platforms. Earned a Full Stack Web
+Development certificate from UCLA. An articulate individual who enjoys connecting with co-workers and is passionate about building applications that assist people to make their lives easier. Looking to bring attention to detail and
+technical skills to a company to grow and become a valuable asset. Strengths in work ethic, problem solving,
+teamwork, creativity and building projects from ideation to execution.
+       
         </p>
        
         <p>
-        I'm
-                    currently studying software and web development at UCLA Bootcamp. Very excited to learn this new
-                    skill.
-                    I'm highly motivated and exceptionally professional having lead many teams throughout my journey.
-
-                    I looking forward to learn as much as possible as a software/web/app developer and apply my
-                    knowledge to make a difference.        </p>
+    Born and raised in Porto Alegre, Brazil, moved to Los Angeles in 2006 looking for a new life and oportunities. 
+                    Passioned about working hard and making a difference on the community. Currently lives with his
+                    girlfriend and his dog Django in the City Of Angels. Loves to code, play guitar, travel and have a good time with his
+                    friends.  </p>
       </div>
     </section>
   );
